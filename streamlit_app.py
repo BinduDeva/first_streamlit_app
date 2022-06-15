17f8c7a33c4df26c61179c473aebbfd128f73fc4
BinduDeva <94528006+BinduDeva@users.noreply.github.com>
2022-06-15 21:49:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free Range Eggs')
-streamlit.text('Avacado Toast 🥑🍞')
+streamlit.text('🥑🍞 Avacado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
